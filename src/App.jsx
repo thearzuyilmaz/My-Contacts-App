48687a3bc9634c6e1878cd5e13b4fa38f43965cd
thearzuyilmaz <the.arzu.yilmaz@gmail.com>
2024-11-25 14:57:22 +0300

    Avatar component update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
 import contacts from './contacts';
+import Avatar from './Avatar';
 
 function App(){
 
     return (<div>
+    
+    <Avatar img = "https://oyster.ignimgs.com/mediawiki/apis.ign.com/avatar-the-last-airbender/1/17/Korra_img.jpg?width=325" />
+
     <h1 className="heading">My Contacts</h1>
     <Card name={contacts[0].name} img={contacts[0].imgURL} tel={contacts[0].phone} email={contacts[0].email}/>
     <Card name={contacts[1].name} img={contacts[1].imgURL} tel={contacts[1].phone} email={contacts[1].email} />
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './index.css';
+import Avatar from './Avatar';
+import Detail from './Detail';
 
 function Card (props){
     return (
     <div className="card">
         <div className="top">
             <h2 className="name">{props.name}</h2>
-            <img className="circle-img"src={props.img}/>
+            <Avatar img= {props.img} />
     </div>
 
     <div className="bottom">
-      <p className="info">{props.tel}</p>
-      <p className="info">{props.email}</p>
+      <Detail detailInfo = {props.tel}/>
+      <Detail detailInfo = {props.email}/>
     </div>
 
     </div>);
